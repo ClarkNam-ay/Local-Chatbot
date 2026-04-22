df7f7de072be913b62beab22b7c1a9555713eb0c
Clark Gwapo <reneilleclark.nam-ay@bisu.edu.ph>
2026-04-22 13:56:36 +0800

    Push

--- Local Chatbot/src/App.tsx
+++ Local Chatbot/src/App.tsx
@@ -224,7 +224,7 @@ export default function App() {
         <div className="flex items-center gap-3 px-5 py-5 border-b border-white/5">
           <img src={logo} alt="ChucksGPT Logo" className="w-10 h-10" />
           <span className="font-semibold text-white tracking-tight">
-            ChucksGPT iehfiehfioweo
+            ChucksGPT iehfiehfioweoweugfuweifgwf
           </span>
         </div>
 
